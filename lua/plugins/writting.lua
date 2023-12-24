fae424aa12202e3e84ea5956a901294f8d3d251e
Darwin Grisales <darwingrisalesg@gmail.com>
2023-12-24 14:31:13 -0500

    :ambulance: [fix][neotree]: show hidding files in neotree

--- lua/plugins/writting.lua
+++ lua/plugins/writting.lua
@@ -11,7 +11,7 @@ return {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function(_, opts)
       local nls = require("null-ls")
-      opts.sources = vim.list_extend(otps.sources, {
+      opts.sources = vim.list_extend(opts.sources, {
         nls.builtins.diagnostics.proselint,
         nls.builtins.code_actions.proselint,
         nls.builtins.diagnostics.alex,
