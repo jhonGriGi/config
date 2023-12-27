7493ab6af5f83b54a3c412ab7a8a4dc2cc7d6624
Darwin Grisales <darwingrisalesg@gmail.com>
2023-12-26 22:39:04 -0500

    :recycle: [refactor][discipline]: remove discipline config

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,9 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 --
-local discipline = require("personal.discipline")
-
-discipline.cowboy()
+-- local discipline = require("personal.discipline")
+--
+-- discipline.cowboy()
 
 local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
